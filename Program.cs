34e2586a4335bc3e9eed8c5cbc46602584795ffc
Ubald1 <127605677+Ubald1@users.noreply.github.com>
2023-05-25 19:48:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int ore = 120;
+int paga = 8;
+
+int guadagno = euroMese(ore, paga);
+
+Console.WriteLine($"Questo mese hai guadagnato: {guadagno}");
+
+static int euroMese(int ore, int paga)
+{
+    Console.WriteLine($"Questo mese prendi:  {ore * paga}");
+    return ore * paga;
+}
